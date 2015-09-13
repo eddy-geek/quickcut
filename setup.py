5adf2e2e8658b66f09b16db1b4936a2d26fd71de
eoubrayrie <edoubrayrie@gmail.com>
2015-09-14 01:03:13 +0200

    fix window icon bug by ncluding it with source package ; get desktop file icon to show on --user installs too

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
+# from distutils.dist import Distribution
+import os
+from pathlib import Path
 from setuptools import setup, find_packages
-from os import path, name
+import sys
 
 """Quickcut's setuptools based setup module.
 
 See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
+http://blog.codekills.net/2011/07/15/lies,-more-lies-and-python-packaging-documentation-on--package_data-/
 """
 
 __author__ = 'eoubrayrie'
 
-here = path.abspath(path.dirname(__file__))
+# dist = Distribution(dict(script_args=sys.argv[1:]))
+# dist.parse_command_line()
+# dist.dump_option_dicts()
+# print(distutils.command.install.install.user_options)
+
+if 'install' in sys.argv and '--user' in sys.argv:
+    prefix = Path(os.getenv('HOME')) / '.local'
+else:
+    prefix = Path('/usr/local')
+
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
 
 REQUIRES = ['pysrt']
 
@@ -18,7 +34,7 @@ try:
     # noinspection PyUnresolvedReferences
     import PyQt5
 except ImportError:
-    if name == "nt":
+    if os.name == "nt":
         # On windows we can use this special repo with binaries:
         REQUIRES.append('python-qt5')
     else:
@@ -27,13 +43,14 @@ except ImportError:
                  https://riverbankcomputing.com/software/pyqt/download5
                  or else quickcut will not start''')
 
-setup(
+s = setup(
     name='QuickCut',
     version='0.0.1',
     url='http://github.com/eddy-geek/quickcut/',
     license='GPL',
     author='Edward Oubrayrie',
-    description='Video & Subtitle Cut on given time-range. ffmpeg/pysrt based.',
+    description='Video & subtitle cut on given time-range. ffmpeg/pysrt based.',
+    long_description=long_description,
     keywords='video, subtitle, editor, avi, mp4, srt, ffmpeg, pysrt',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=REQUIRES,
@@ -47,15 +64,15 @@ setup(
         'quickcut': {
             'Name': 'QuickCut',
             'GenericName': 'Video & Subtitle Cut',
-            'Categories': 'Multimedia',
+            'Categories': 'Utility;Application;Multimedia;QT;KDE;GNOME',
+            'Icon': str(prefix / 'share/icons/hicolor/128x128/apps/quickcut.png')
         },
     },
-    # If there are data files included in your packages that need to be installed
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
+    package_data={
+        '': ['*.png'],
+    },
     data_files=[  # Paths are relative to '<sys.prefix>'
-        ('share/applications', ['quickcut.desktop']),
+        # ('share/applications', ['quickcut.desktop']),  # superseded by install_freedesktop
         ('share/icons/hicolor/128x128/apps', ['quickcut/quickcut.png']),
     ],
     classifiers=[
@@ -71,5 +88,9 @@ setup(
         'Topic :: Multimedia :: Video',
         'Topic :: Utilities',
     ],
-    include_package_data=True,
 )
+
+
+if 'install_data' in s.command_obj:
+    print('Installed to', s.command_obj['install_data'].install_dir)  # e.g. ~/.local
+    # icon path could be cutsomized here too
